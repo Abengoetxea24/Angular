efe89d7d6823ccc4c3acb4095859a91540f4bb2b
Janire8 <janirecruz8@gmail.com>
2025-01-21 14:17:58 +0100

    solucionado no se muestra el navbar en el login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Reto-3';
+
+  /* Variable para controlar la visibilidad del login en el navbar */
+  mostrarNavbar:boolean = true;
+
+  //Controlar la visibilidad (.suscribe) llamando al método
+  constructor(private router: Router) {
+    this.router.events.subscribe(() => {
+      this.updateMostrarNavbar();
+    });
+  }
+
+  // Actualiza la visibilidad del navbar según la ruta
+  private updateMostrarNavbar(): void {
+    const currentRoute = this.router.url; //Si la ruta no es /login se muestra el navbar
+    this.mostrarNavbar = currentRoute !== '/Login'; //Ocultar navbar en login
+  }
 }
