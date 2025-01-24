1fe9733f62605728938dc9e405dbc6d2b55c7465
Aimar Bengoetxea Olano <aimarbengoetxeaolano@MacBook-Air-de-Aimar.local>
2025-01-24 10:11:47 +0100

    TODO Funciona

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,6 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './Components/home/home.component';
 import { LoginComponent } from './Components/login/login.component';
 import { SobreNosotrosComponent } from './Components/sobre-nosotros/sobre-nosotros.component';
-import { RescatadosPubliComponent } from './Components/rescatados-publi/rescatados-publi.component';
-import { DonateComponent } from './Components/donate/donate.component';
 import { AdminComponent } from './Components/admin/admin.component';
 import { NavbarComponent } from './Components/navbar/navbar.component';
 import { FormRescatadosComponent } from './Components/admin/form-rescatados/form-rescatados.component';
@@ -21,8 +19,7 @@ import { FormDonacionComponent } from './Components/form-donacion/form-donacion.
     HomeComponent,
     LoginComponent,
     SobreNosotrosComponent,
-    RescatadosPubliComponent,
-    DonateComponent,
+    
     AdminComponent,
     NavbarComponent,
     FormRescatadosComponent,
